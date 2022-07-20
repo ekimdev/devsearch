47dffb340c76c73ed30d58cefc5d02285351aedb
Rodrigo Acosta <rodrigoacosta444@gmail.com>
2022-07-19 23:59:24 -0300

    feat: add serializers class for models

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import views
 
 
 urlpatterns = [
+    path('users/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('users/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
     path('', views.get_routes),
     path('projects/', views.get_projects),
     path('projects/<str:pk>/', views.get_project),
